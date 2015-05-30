99f1c2d2e2ab1a0e9e9cd5f8d5a5bc49d32eea19
Rainer <rainer@websimple.be>
2015-05-30 11:21:16 +0200

    alias nécessaires  pour le from

--- action/editer_reservation_communication.php
+++ action/editer_reservation_communication.php
@@ -72,16 +72,16 @@ function reservation_communication_inserer($id_parent = null, $set = null) {
 
       case  'evenement' :
         $from = 'spip_reservations_details AS rd
-          LEFT JOIN spip_reservations USING (id_reservation)
-          LEFT JOIN spip_auteurs USING (id_auteur)';
+          LEFT JOIN spip_reservations AS res USING (id_reservation)
+          LEFT JOIN spip_auteurs AS aut USING (id_auteur)';
         $where[] = 'rd.id_evenement=' . $id_objet;
         break;
 
       case 'article' :
         $from = 'spip_evenements AS e
           LEFT JOIN spip_reservations_details USING (id_evenement)
-          LEFT JOIN spip_reservations USING (id_reservation)
-          LEFT JOIN spip_auteurs USING (id_auteur)';
+          LEFT JOIN spip_reservations AS res USING (id_reservation)
+          LEFT JOIN spip_auteurs AS aut USING (id_auteur)';
         $where[] = 'e.id_article=' . $id_objet;
 
         break;
@@ -90,8 +90,8 @@ function reservation_communication_inserer($id_parent = null, $set = null) {
         $from = 'spip_articles AS a
           LEFT JOIN spip_evenementsUSING (id_article)
           LEFT JOIN spip_reservations_details USING (id_evenement)
-          LEFT JOIN spip_reservations USING (id_reservation)
-          LEFT JOIN spip_auteurs USING (id_auteur)';
+          LEFT JOIN spip_reservations AS res USING (id_reservation)
+          LEFT JOIN spip_auteurs AS aut USING (id_auteur)';
         $where[] = 'a.id_rubrique=' . $id_objet;
 
         break;
